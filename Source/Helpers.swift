3d763ec2d00bca4b2b3a10a792acdf93105a7373
Clément Le Provost <clement.leprovost@algolia.com>
2016-06-29 09:17:54 +0200

    Fix compilation in helpers
    The whole type system in collections is now really picky… :)

--- Source/Helpers.swift
+++ Source/Helpers.swift
@@ -63,8 +63,8 @@ extension MutableCollection where Index == Int {
         // Empty and single-element collections don't shuffle.
         if count < 2 { return }
         
-        for i in 0..<count - 1 {
-            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+        for i in 0..<self.endIndex {
+            let j = Int(arc4random_uniform(UInt32(self.endIndex - i))) + i
             guard i != j else { continue }
             swap(&self[i], &self[j])
         }
